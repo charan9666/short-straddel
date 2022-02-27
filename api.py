23d132b0f5aa859f75cfaca9c2fd0d049311d93b
charan9666 <charansai.scs@gmail.com>
2022-02-27 10:57:18 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,17 +1,9 @@
-from alice_blue import *
-from datetime import date
-from datetime import time
-import datetime
-import time
-import traceback
-import math
-
-user="251445"
-pwd="Charan@9666"
-secret="AHAOJZ1fSWhBsENrITJiyKHScg1PPyRtSKHFfY3KlOVjoqFz7bBv8WVp12GFPMqT"
-app="gENcWukY6p"
-twofa ="1998"
 
 
-x= datetime.datetime.now()
-print(x)
+user=" " 
+pwd=" "
+secret=" "
+app=" "
+twofa =" "
+
+
